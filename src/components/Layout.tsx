cc917c560f6be2f2ebaf6c06771c0af680c66676
Auly <ryul8465@naver.com>
2025-08-26 14:40:34 +0900

    프로필 드롭다운 hover 유지 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -78,7 +78,7 @@ function Layout({ children }: LayoutProps) {
                     d="M1.78 21.25c.382-4.758 4.364-8.5 9.22-8.5h1c4.856 0 8.838 3.742 9.22 8.5H1.78z"></path>
                 </svg>
               </button>
-              <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 hidden group-hover:block"
+              <div className="absolute right-0 z-10 mt-0 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 hidden group-hover:block"
                   role="menu" aria-orientation="vertical" tabIndex={-1}>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                   role="menuitem" tabIndex={-1}>Profile</a>
